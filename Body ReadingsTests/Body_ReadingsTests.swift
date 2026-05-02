0d067090ee1d1133167438e17e85000427c145d2
Michael Fluharty <michael@fluharty.com>
2026-05-02 18:56:22 -0500

    Initial Commit

---
+++ Body ReadingsTests/Body_ReadingsTests.swift
@@ -0,0 +1,19 @@
+//
+//  Body_ReadingsTests.swift
+//  Body ReadingsTests
+//
+//  Created by Michael Fluharty on 5/2/26.
+//
+
+import Testing
+@testable import Body_Readings
+
+struct Body_ReadingsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
